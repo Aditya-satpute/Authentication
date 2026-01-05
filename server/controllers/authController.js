674c6f0dd36d1aa55c468dda1ddd791aa12b96dd
unknown <adityasatpute402@gmail.com>
2026-01-05 23:42:09 +0530

    Created server side controller functions to generate and verify otp and send that through mails

---
+++ server/controllers/authController.js
@@ -0,0 +1,166 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
+
+export const register = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        return res.json({success: false, message: 'Missing Details'})
+    }
+
+    try{
+        const existingUser = await userModel.findOne({email})   //if there is already such email exist in the database
+        if(existingUser) {
+            return res.json({success: false, message: 'User already exists'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new userModel({name, email, password: hashedPassword})
+        await user.save();
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000    //(7 days in ms)
+        });
+
+        // Sending welcome email
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email, 
+            subject: 'Welcome to Leorex',
+            text: `Welcome to Leorex website. Your account has been created with email id: ${email}`
+        }
+
+        await transporter.sendMail(mailOptions);
+
+        return res.json({success: true});
+
+    } catch (error){
+        res.json({success: false, message:error.message})
+    }
+}
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return res.json({success:false, message: 'Email and Password are required'});
+    }
+
+    try{
+        const user = await userModel.findOne({email});
+
+        if(!user){
+            return res.json({success: false, message: 'Invalid Email'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.json({success: false, message: 'Invalid Password'});
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none' : 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000    //(7 days in ms)
+        });
+
+        return res.json({success: true});
+
+
+    } catch (error){
+        return res.json({success: false, message: error.message});
+    }
+}
+
+export const logout = async (req, res)=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none' : 'strict',
+        })
+
+        return res.json({success: true, message: 'Logged Out Successfully'});
+
+    } catch (error){
+        return res.json({success: false, message: error.message});
+    }
+}
+
+// Send Verification otp to the User's Email
+export const sendVerifyOtp = async (req, res)=>{
+    try{
+        const {userId} = req.body;
+
+        const user = await userModel.findById(userId);
+
+        if(user.isAccountVerified){
+            return res.json({success: false, message: 'Account is already verified'})
+        }
+
+        const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+        user.sendVerifyOtp = otp;
+        user.sendVerifyOtpExpireAt = Date.now() + 24*60*60*1000;
+
+        await user.save();
+
+        const mailOption = {
+            from: process.env.SENDER_EMAIL,
+            to: user.email, 
+            subject: 'Account Verification OTP',
+            text: `Your OTP is ${otp}. Verify your account using this OTP.`
+        }
+
+        await transporter.sendMail(mailOption);
+
+        res.json({success: true, message: 'Verification OTP sent on email'})
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+// Verify OTP 
+export const verifyEmail = async(req, res)=>{
+    const {userId, otp} = req.body;
+
+    if(!userId || !otp){
+        return res.json({success: false, message: 'Missing Details'})
+    }
+
+    try {
+        const user = await userModel.findById(userId);
+
+        if(!user) {
+            return res.json({success: false, message: 'User Not Found'})
+        }
+        if(user.verifyOtp === '' || user.verifyOtp !== otp) {
+            return res.json({success: false, message: 'Invalid OTP'})
+        }
+        if(user.sendVerifyOtpExpireAt < Date.now()){
+            return res.json({success: false, message: 'OTP Expired'})
+        }
+
+        user.isAccountVerified = true;
+        user.verifyOtp = '';
+        user.sendVerifyOtpExpireAt = 0;
+
+        await user.save();
+        return res.jason({success: true, message: 'Email verified successfully'})
+
+    } catch (error) {
+        return res.json({success: false, message: error.message})
+    }
+}
--- server/server.md
+++ server/server.md
@@ -1,2 +1,8 @@
 npm init -y
 npm install express cors dotenv nodemon jsonwebtoken mongoose bcryptjs nodemailer cookie-parser
+
+
+server.js -> models -> controllers -> routes
+
+nodemailer
+we want smtp so first login to Brevo-Login
